59060c1cd682f7ce874a4b55504d8ab19d3f65e6
Gabriel Freitas <122943129+Gabriel19p@users.noreply.github.com>
2023-07-01 00:59:34 -0300

    Delete ProjetoFinal directory

--- ProjetoFinal/src/main/java/Repository/AlunoRepository.java
+++
@@ -1,8 +0,0 @@
-package Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import model.Aluno;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-    Aluno findByMatricula(String matricula);
-}
