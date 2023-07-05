77945b102d210686359a91736e92d496c2a413c2
Gabriel Freitas <122943129+Gabriel19p@users.noreply.github.com>
2023-07-05 15:00:36 -0300

    Add files via upload

---
+++ Devec(Projeto) -AcademiaAtos/src/main/java/com/projetofinal/controller/AlunoController.java
@@ -0,0 +1,62 @@
+package com.projetofinal.controller;
+
+import com.projetofinal.model.Aluno;
+import com.projetofinal.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/aluno")
+public class AlunoController {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @GetMapping("/adicionarAluno")
+    public String adicionarAluno(Model model) {
+        model.addAttribute("aluno", new Aluno());
+        return "aluno/adicionarAluno";
+    }
+
+    @PostMapping("/adicionarAluno")
+    public String adicionarAluno(@ModelAttribute Aluno aluno, Model model) {
+        alunoRepository.save(aluno);
+        listarAluno(model);
+        return "aluno/listarAluno";
+    }
+
+    @GetMapping("/listarAluno")
+    public String listarAluno(Model model) {
+        List<Aluno> lista = this.alunoRepository.findAll();
+        model.addAttribute("listarAluno", lista);
+        return "aluno/listarAluno";
+    }
+
+    @GetMapping("/alterarAluno/{id}")
+    public String alterarAluno(@PathVariable Long id, Model model) {
+        Aluno aluno = alunoRepository.findById(id).get();
+        model.addAttribute("aluno",aluno);
+        return"aluno/alterarAluno";
+    }
+
+    @PostMapping("/alterarAluno")
+    public String alteraProduto(@ModelAttribute Aluno aluno,Model model) {
+        alunoRepository.save(aluno);
+        listarAluno(model);
+        return "aluno/listarAluno";
+    }
+
+    @GetMapping("/excluirAluno/{id}")
+    public String excluirAluno(@PathVariable("id") Long id, Model model) {
+        alunoRepository.deleteById(String.valueOf(id));
+        listarAluno(model);
+        return "aluno/listarAluno";
+    }
+}
+
+
---
+++ Devec(Projeto) -AcademiaAtos/src/main/java/com/projetofinal/repository/DisciplinaRepository.java
@@ -0,0 +1,8 @@
+package com.projetofinal.repository;
+
+import com.projetofinal.model.Disciplina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
+}
+
